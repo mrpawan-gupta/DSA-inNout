c1391c58d7d8ec834cd2c58aff6a766f2fe45f64
Pawan Gupta <guptapawanro2017@gmail.com>
2021-09-01 00:19:37 +0530

    Array Input Output

---
+++ 1]. Data Structures./01]. Arrays/C++/_0001]_Array_IO.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    
+    cout << "Enter the no of elements :" ;
+    int num ; cin >> num;
+    cout << "Enter the value :" <<endl; 
+    int arr[num];
+    for(int i=0; i<num; i++){
+        cin >> arr[i] ;
+    }
+    cout << "The values in the array are :" ;
+    for(int i=0; i<num;++i){
+        cout << arr[i] << " ";
+    }
+    
+    return 0;
+}
