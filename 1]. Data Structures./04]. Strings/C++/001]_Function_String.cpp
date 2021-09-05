a3caf6190c686181a45d8f116ed7e9b3e3027755
Pawan Gupta <guptapawanro2017@gmail.com>
2021-09-05 20:41:18 +0530

    String Operations

---
+++ 1]. Data Structures./04]. Strings/C++/001]_Function_String.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string str1 = "pawan";
+    string str2 = "pawan";
+
+    cout<<str1.append(str2)<<endl; // concatinate two string
+    cout <<str1.compare(str2) <<endl; 
+    
+    
+}
