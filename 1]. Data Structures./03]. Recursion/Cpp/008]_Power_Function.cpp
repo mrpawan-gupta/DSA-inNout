f48fbfbe2c98a542e5f04cb06ff18603692f6caa
Pawan Gupta <guptapawanro2017@gmail.com>
2021-09-22 22:30:05 +0530

    power Function

---
+++ 1]. Data Structures./03]. Recursion/Cpp/008]_Power_Function.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int calculatePower(int base, int powerRaised)
+{
+    if (powerRaised != 0)
+        return (base*calculatePower(base, powerRaised-1));
+    else
+        return 1;
+}
+
+int main()
+{
+    int base, powerRaised, result;
+
+    cout << "Enter base number: ";
+    cin >> base;
+
+    cout << "Enter power number(positive integer): ";
+    cin >> powerRaised;
+
+    result = calculatePower(base, powerRaised);
+    cout << base << " ^ " << powerRaised << " = " << result;
+
+    return 0;
+}
+
