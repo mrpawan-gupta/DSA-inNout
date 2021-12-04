bdc279d5950cd42c66b1e61255e5e7a4998d1a1c
Pawan Gupta <guptapawanro2017@gmail.com>
2021-12-04 16:04:35 +0530

    String IO

--- 1]. Data Structures./04]. Strings/Cpp/002]_String_Declarations.cpp
+++ 1]. Data Structures./04]. Strings/Cpp/002]_String_Declarations.cpp
@@ -4,4 +4,17 @@ using namespace std ;
 
 int main(){
     string str1 = "Hello";
+    cout << str1 << endl;
+
+    // Input Output String
+
+    string s;
+    cin >> s; // single string
+
+    string s2;
+    getline(cin, s2); // group of string
+
+    // print strings 
+    cout << s << endl;
+    cout << s2 << endl; 
 }
