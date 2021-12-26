d96158fda234b69d6c04043b918867945b02f2e9
Pawan Gupta <guptapawanro2017@gmail.com>
2021-12-26 23:52:57 +0530

    Basic Heap Implementation

--- 1]. Data Structures./13].Heap/Cpp/01].BasicHeap.cpp
+++ 1]. Data Structures./13].Heap/Cpp/01].BasicHeap.cpp
@@ -1,14 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Heap
-{
+class Heap{
 public:
     Heap(int size) : current_size_(0), size_(size), arr_(new int[size]) {}
 
     // Insert a new value to the heap
-    void insert(const int value)
-    {
+    void insert(const int value){
         if (current_size_ < size_)
         {
             arr_[current_size_] = value;
@@ -21,14 +19,12 @@ public:
     }
 
     // Retrieves the value store at the root of the tree
-    int top()
-    {
+    int top(){
         return arr_[0];
     }
 
     // Extract the root value of the tree and return it as result
-    int pop()
-    {
+    int pop(){
         swap(arr_[0], arr_[current_size_ - 1]);
         heapify(current_size_ - 1, 0);
         current_size_--;
@@ -38,8 +34,7 @@ public:
 
 private:
     // Convert a simple array in a Heap
-    void convertArrayIntoHeap()
-    {
+    void convertArrayIntoHeap(){
         // Just need to process the node with children
         for (int i = current_size_ / 2 - 1; i >= 0; i--)
         {
@@ -52,8 +47,7 @@ private:
     than the node value, swap the content between them and recursively does the
     same with the swaped node.
   */
-    void heapify(int size, int i)
-    {
+    void heapify(int size, int i){
         int max_value = i;
         int left = i * 2 + 1;
         int right = i * 2 + 2;
@@ -62,8 +56,7 @@ private:
             max_value = left;
         if (right < size && arr_[right] > arr_[max_value])
             max_value = right;
-        if (max_value != i)
-        {
+        if (max_value != i){
             swap(arr_[i], arr_[max_value]);
             heapify(size, max_value);
         }
@@ -74,20 +67,17 @@ private:
     int size_;
 };
 
-int main()
-{
+int main(){
     int size, value;
     cin >> size;
 
     Heap heap(size);
-    for (size_t i = 0; i < size; i++)
-    {
+    for (size_t i = 0; i < size; i++){
         cin >> value;
         heap.insert(value);
     }
 
-    for (size_t i = 0; i < size; i++)
-    {
+    for (size_t i = 0; i < size; i++){
         cout << heap.pop() << ' ';
     }
 
