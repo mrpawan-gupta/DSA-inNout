7f3ac83f84c5a7042204bff7a9637314604637e0
Pawan Gupta <guptapawanro2017@gmail.com>
2022-02-06 23:46:37 +0530

    Solution Count the number of possible triangles

---
+++ 1]. Data Structures./00]. Maths-BitMagic/013]_Count_the_number_of_possible_triangles.cpp
@@ -0,0 +1,56 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+public:
+    // Function to count the number of possible triangles.
+    int findNumberOfTriangles(int arr[], int n)
+    {
+        // code here
+
+        sort(arr, arr + n);
+        int count = 0;
+        for (int i = n - 1; i >= 2; i--)
+        {
+            int l = 0;
+            int r = i - 1;
+
+            while (l < r)
+            {
+                if (arr[l] + arr[r] > arr[i])
+                {
+                    count += (r - l);
+                    r--;
+                }
+                else
+                {
+                    l++;
+                }
+            }
+        }
+
+        return count;
+    }
+};
+
+// { Driver Code Starts.
+
+int main()
+{
+    int T;
+    cin >> T;
+    while (T--)
+    {
+        int n;
+        cin >> n;
+        int arr[n];
+        for (int i = 0; i < n; i++)
+            cin >> arr[i];
+        Solution ob;
+        cout << ob.findNumberOfTriangles(arr, n) << endl;
+    }
+    return 0;
+} // } Driver Code Ends
