afbb6928a38280c2b91edbd12ef7438f7482688b
Pawan Gupta <guptapawanro2017@gmail.com>
2022-04-26 23:04:44 +0530

    String Palindrome Added

--- 1]. Data Structures./04]. Strings/Cpp/009]_StringPalindrome.cpp
+++ 1]. Data Structures./04]. Strings/Cpp/009]_StringPalindrome.cpp
@@ -1,7 +1,46 @@
+/*
+    Solution By :- Pawan Gupta [ CF - Mr.pawan_gupta, CC - guptapawan ]
+*/
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-    
-    return 0 ;
+class Solution{
+    public:
+        bool checkPalindrome(string s){
+            int i = 0;
+            int j = s.size()-1;
+
+            while(i<j){
+                if(s[i]!=s[j]){
+                    return false;
+                }
+                i++;
+                j--;
+            }
+            return true;
+        }
+};
+
+int main(int argc, char const *argv[]){
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    //cin >> t;
+
+    string String;
+    cin >> String;
+
+    while(t--){
+        Solution ob;
+        bool check = ob.checkPalindrome(String);
+        if(check){
+            cout << "String " << String <<" is Palindrome";
+        }else{
+            cout << "String " << String <<" is not Palindrome";
+        }
+    }
+
+    return 0;
 }
