23645ce8467bb28ef6daeec29f47378efd6940c6
Pawan Gupta <guptapawanro2017@gmail.com>
2022-05-21 23:53:22 +0530

    Solution Merge Overlaping Intervals

---
+++ 1]. Data Structures./01]. Arrays/Cpp/_0038]_Merge_Intervals.cpp
@@ -0,0 +1,51 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    vector<vector<int>> overlappedInterval(vector<vector<int>>& intervals) {
+         // Code here
+        vector<vector<int>> mergedIntervals;
+        if(intervals.size() == 0) {
+            return mergedIntervals; 
+        }
+        sort(intervals.begin(), intervals.end());
+        vector<int> tempInterval = intervals[0]; 
+        
+        for(auto it : intervals) {
+            if(it[0] <= tempInterval[1]) {
+                tempInterval[1] = max(it[1], tempInterval[1]); 
+            } else {
+                mergedIntervals.push_back(tempInterval); 
+                tempInterval = it; 
+            }
+        }
+        mergedIntervals.push_back(tempInterval);
+        return mergedIntervals; 
+    }
+};
+
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		int n;
+		cin >> n;
+		vector<vector<int>>Intervals(n);
+		for(int i = 0; i < n; i++){
+			int x, y;
+			cin >> x >> y;
+			Intervals[i].push_back(x);
+			Intervals[i].push_back(y);
+		}
+		Solution obj;
+		vector<vector<int>> ans = obj.overlappedInterval(Intervals);
+		for(auto i: ans){
+			for(auto j: i){
+				cout << j << " ";
+			}
+		}
+		cout << "\n";
+	}
+	return 0;
+} 
