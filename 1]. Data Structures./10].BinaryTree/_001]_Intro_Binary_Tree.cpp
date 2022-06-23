a8ee5a2dfb8ec362347aafa8975e656a2bb0ec98
Pawan Gupta <guptapawanro2017@gmail.com>
2022-06-23 17:57:14 +0530

    Intro to Binary Tree

---
+++ 1]. Data Structures./10].BinaryTree/_001]_Intro_Binary_Tree.cpp
@@ -0,0 +1,49 @@
+/*
+    Solution By :- Pawan Gupta ( पवन गुप्ता )[ CF - Mr.pawan_gupta, CC - guptapawan ]
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+class TreeNode{
+    public:
+        int data;
+        TreeNode* left;
+        TreeNode* right;
+
+        TreeNode(int value){
+            data = value; // value id being Initialized to the Node.
+            left = NULL;
+            right = NULL;
+            // left & right child are intialized with Null value.
+        }
+};
+
+int main(int argc, char const *argv[]){
+
+    // creating the Root Node of the tree
+    TreeNode* Node = new TreeNode(10);
+    /* following is the tree after above statement
+ 
+             10
+            / \
+        NULL   NULL
+    */
+    Node->left = new TreeNode(20);
+    Node->right = new TreeNode(30);
+    Node->left->left = new TreeNode(40);
+    Node->left->right = new TreeNode(50);
+    Node->right->left = new TreeNode(60);
+    Node->right->right = new TreeNode(70);
+    /* following is the tree after above statement
+ 
+             10
+            / \
+          20   30
+         / \    / \
+        40  50  60 70
+    */
+
+
+
+    return 0;
+}
